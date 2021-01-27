bcba9eec96ffe200f7b49786e41f28de872ff3f2
SherstnevVasiliy <74350736+SherstnevVasiliy@users.noreply.github.com>
2021-01-28 00:36:25 +0300

    upload final project

---
+++ precache-manifest.eeb5b4050770b6973d233a7865973961.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01df3a8376684a7eb34464d5482cbf65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08a06f1b15b3a1ff0934",
+    "url": "/static/css/main.8e7f13ec.chunk.css"
+  },
+  {
+    "revision": "58f15925e29368b4c683",
+    "url": "/static/js/2.7a407c44.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.7a407c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08a06f1b15b3a1ff0934",
+    "url": "/static/js/main.13bcba46.chunk.js"
+  },
+  {
+    "revision": "add741e403bec697532d",
+    "url": "/static/js/runtime-main.05d0e1c6.js"
+  },
+  {
+    "revision": "22baa51c93e3c3f3e751359302d4cc77",
+    "url": "/static/media/bannermain.22baa51c.png"
+  },
+  {
+    "revision": "aa69f806fa8da7534dc548bab8700711",
+    "url": "/static/media/bizness.aa69f806.jpg"
+  },
+  {
+    "revision": "9c950e6b9beeda3236d31e8855d4e808",
+    "url": "/static/media/granta.9c950e6b.jpg"
+  },
+  {
+    "revision": "0d5f5f0c08cd72234363364932bb91cb",
+    "url": "/static/media/kamaz.0d5f5f0c.jpg"
+  },
+  {
+    "revision": "b5f0fe4c7ab7b5888d6b606e9d3e98f2",
+    "url": "/static/media/mazz.b5f0fe4c.jpg"
+  },
+  {
+    "revision": "af81fb3ac251b9aa1a01574494045754",
+    "url": "/static/media/next.af81fb3a.jpg"
+  },
+  {
+    "revision": "337cec38b456108792265da40d554626",
+    "url": "/static/media/niva.337cec38.jpg"
+  },
+  {
+    "revision": "1ae19dd38d771d1ec19a9732092dd12b",
+    "url": "/static/media/nivashev.1ae19dd3.jpg"
+  },
+  {
+    "revision": "a25816a1831ad561713259924c82cf12",
+    "url": "/static/media/patriot.a25816a1.jpg"
+  },
+  {
+    "revision": "ee548354e048e6124150a3cb824dcaf5",
+    "url": "/static/media/slider_1.ee548354.jpg"
+  },
+  {
+    "revision": "fadec8d0101e3ec3e71b96734afa9cca",
+    "url": "/static/media/slider_2.fadec8d0.jpg"
+  },
+  {
+    "revision": "d6fc7efd7801f8b0839dd226ea07ca23",
+    "url": "/static/media/slider_3.d6fc7efd.jpg"
+  },
+  {
+    "revision": "639768e680058aef558fb66af017f4b4",
+    "url": "/static/media/uralnext.639768e6.jpg"
+  },
+  {
+    "revision": "ad4f3f486498b346890a56f80a4cabb4",
+    "url": "/static/media/valday.ad4f3f48.jpg"
+  },
+  {
+    "revision": "e2ddc4e3ea311a1cfe43f010ba86b56e",
+    "url": "/static/media/vesta.e2ddc4e3.jpg"
+  },
+  {
+    "revision": "ecd4b3fc19632feec4c5c0b2e72d982c",
+    "url": "/static/media/xray.ecd4b3fc.jpg"
+  }
+]);
